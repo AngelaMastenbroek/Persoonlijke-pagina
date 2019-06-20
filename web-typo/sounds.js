777362c6e2faf5dd63ad077f6e5a7f9951b4c919
Angela <34062624+AngelaMastenbroek@users.noreply.github.com>
2019-06-20 17:21:51 +0200

    Add files via upload

---
+++ web-typo/sounds.js
@@ -0,0 +1,18 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+    .1,
+    9,
+    12,
+    15.5,
+    16.5,
+    24.5,
+    25.8,
+    31,
+    32,
+    34,
+    86,
+    103,
+    106
+];
+
